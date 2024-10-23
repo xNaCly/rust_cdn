0f7f95b83a205be98fb1dc88a96ac203ef35370e
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-10-23 14:28:15 +0200

    rework and storage of files

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::{Context, Result};
 use http_body_util::combinators::BoxBody;
 use serde::Serialize;
+use tokio::fs;
 
+use core::str;
 use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 use http_body_util::{BodyExt, Full};
 use hyper::body::Bytes;
@@ -13,16 +17,43 @@ use hyper::{Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
-#[derive(Serialize)]
-struct File<'file> {
-    name: &'file str,
+type FileStore = Arc<Mutex<HashMap<String, File>>>;
+
+#[derive(Serialize, Clone)]
+struct File {
+    name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    content: Option<String>,
 }
 
 #[derive(Serialize)]
 struct CdnResponse<'response> {
     msg: &'response str,
     #[serde(skip_serializing_if = "Option::is_none")]
-    files: Option<Vec<File<'response>>>,
+    files: Option<Vec<File>>,
+}
+
+fn init_store() -> Result<FileStore> {
+    let store = Arc::new(Mutex::new(HashMap::new()));
+    let mut lock = store.lock().unwrap();
+    std::fs::read_dir("./store")?
+        .flatten()
+        .filter(|e| !e.metadata().unwrap().is_dir())
+        .map(|file: std::fs::DirEntry| -> Result<File> {
+            Ok(File {
+                name: file.file_name().to_str().unwrap().to_string(),
+                content: String::from_utf8(std::fs::read(file.path())?).ok(),
+            })
+        })
+        .flatten()
+        .for_each(move |file| {
+            lock.insert(file.name.clone(), file);
+        });
+    println!(
+        "cdn: Found {} File(s) on disk, loading into memory store",
+        store.lock().unwrap().len()
+    );
+    Ok(store)
 }
 
 fn full<T: Into<Bytes>>(chunk: T) -> http_body_util::combinators::BoxBody<Bytes, std::io::Error> {
@@ -31,60 +62,124 @@ fn full<T: Into<Bytes>>(chunk: T) -> http_body_util::combinators::BoxBody<Bytes,
         .boxed()
 }
 
+/// response generates a Result<Response, ...> from the http statuscode and a message, containing
+///     { msg: msg }
+fn response(code: StatusCode, msg: &str) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
+    Ok(Response::builder()
+        .status(code)
+        .body(full(serde_json::to_vec(&CdnResponse { msg, files: None })?))?)
+}
+
 async fn response_handler(
     req: Request<hyper::body::Incoming>,
+    db_handle: FileStore,
 ) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/files") => Ok(all()),
-        (&Method::POST, "/file") => Ok(upload(req).await),
-        _ => Ok(catch_all()),
+        (&Method::GET, "/files") => all(db_handle).await,
+        (&Method::POST, "/file") => upload(req, db_handle).await,
+        // TODO: this does not work..., needs a fix, idk how to yet tho
+        (&Method::GET, "/file/") => download(req, db_handle).await,
+        _ => response(StatusCode::NOT_FOUND, "Not Found"),
     }
 }
 
-async fn upload(req: Request<hyper::body::Incoming>) -> Response<BoxBody<Bytes, std::io::Error>> {
+async fn upload(
+    req: Request<hyper::body::Incoming>,
+    db_handle: FileStore,
+) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
     let whole_body = req.collect().await.unwrap().to_bytes();
     // process path param
     let params = form_urlencoded::parse(whole_body.as_ref())
         .into_owned()
         .collect::<HashMap<String, String>>();
 
-    if let Some(filename) = params.get("filename") {
-        println!("{}", filename);
+    if !(params.contains_key("name") && params.contains_key("content")) {
+        return response(
+            StatusCode::BAD_REQUEST,
+            "Missing name or content in request body",
+        );
     }
 
-    todo!()
+    let file = File {
+        name: params.get("name").unwrap().to_string(),
+        content: { Some(params.get("content").unwrap_or(&String::from("")).clone()) },
+    };
+    let mut lock = db_handle.lock().unwrap();
+    let filename = file.name.clone();
+
+    std::fs::write(
+        Path::new(".").join("store").join(&filename),
+        file.content.clone().unwrap(),
+    )?;
+
+    lock.insert(filename, file);
+
+    response(
+        StatusCode::CREATED,
+        &format!("Stored file '{}'", params.get("name").unwrap()),
+    )
 }
 
-fn all() -> Response<BoxBody<Bytes, std::io::Error>> {
-    let response = CdnResponse {
-        msg: "Got files",
-        files: None,
+async fn download(
+    req: Request<hyper::body::Incoming>,
+    db_handle: FileStore,
+) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
+    dbg!(req.uri().path().split("/"));
+    let file_name = match req.uri().path().split("/").last() {
+        Some(str) => str,
+        None => return response(StatusCode::BAD_REQUEST, "No file path given"),
     };
 
-    Response::builder()
+    // edge case if only /file is called
+    if file_name == "file" {
+        return response(StatusCode::BAD_REQUEST, "No file path given");
+    }
+
+    let handle = db_handle.lock().unwrap();
+    if let Some(file) = handle.get(file_name) {
+        todo!("return file here")
+    }
+
+    response(
+        StatusCode::NOT_FOUND,
+        &format!("File '{}' not found in store", file_name),
+    )
+}
+
+async fn all(db: FileStore) -> Result<Response<BoxBody<Bytes, std::io::Error>>> {
+    let handle = db.lock().unwrap();
+    let files = handle
+        .keys()
+        .map(|key| File {
+            name: String::from(key),
+            content: None,
+        })
+        .collect::<Vec<File>>();
+    let response = CdnResponse {
+        msg: match &files.len() {
+            0 => "Got no files",
+            _ => &format!("Got {} files", files.len()),
+        },
+        files: Some(files),
+    };
+
+    Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(full(serde_json::to_vec(&response).unwrap()))
-        .unwrap()
+        .body(full(serde_json::to_vec(&response).unwrap()))?)
 }
 
-/// HTTP status code 404
-fn catch_all() -> Response<BoxBody<Bytes, std::io::Error>> {
-    let response = CdnResponse {
-        msg: "Not found",
-        files: None,
-    };
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(full(serde_json::to_vec(&response).unwrap()))
-        .unwrap()
-}
-
+/// rust_cdn works by making all writes on disk but all reads are performed from the in memory FileStore data type, this makes reads extremly fast
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = TcpListener::bind(addr)
         .await
         .context("Failed to start the server")?;
+
+    fs::create_dir_all("./store")
+        .await
+        .context("Failed to create file store")?;
+    let db = init_store()?;
 
     loop {
         let (stream, _) = listener
@@ -93,10 +188,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             .context("Failed to await stream accepting")?;
 
         let io = TokioIo::new(stream);
+        let db_handle = db.clone();
 
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(response_handler))
+                .serve_connection(
+                    io,
+                    service_fn(move |req| {
+                        let method = (&req).method().clone();
+                        let path = (&req).uri().path().to_string();
+                        let res = response_handler(req, Arc::clone(&db_handle));
+                        async move {
+                            let r = res.await;
+                            if let Ok(ok) = &r {
+                                println!("| {} | [{}] '{}' ", ok.status().as_u16(), method, path);
+                            }
+                            r
+                        }
+                    }),
+                )
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
--- test.ts
+++ test.ts
@@ -1,9 +1,18 @@
-await fetch("http://localhost:8080/file", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  body: new URLSearchParams({
-    filename: "helloWorld.txt",
-  }).toString(),
-});
+// console.log(
+//     await (await fetch("http://localhost:8080/file", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: new URLSearchParams({
+//             name: `helloWorld-${
+//                 (Math.random() + 1).toString(36).substring(7)
+//             }.txt`,
+//             content: "Hello World",
+//         }).toString(),
+//     })).json(),
+// );
+console.log(
+    await (await fetch("http://localhost:8080/file/helloWorld-xl4rp.txt"))
+        .json(),
+);
